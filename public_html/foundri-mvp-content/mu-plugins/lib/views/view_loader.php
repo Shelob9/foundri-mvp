023e6ce82808f1c2a2b8072666f7760d9b7b6485
Josh Pollock <jpollock412@gmail.com>
2015-05-11 17:12:35 -0400

    prepare to have views

---
+++ public_html/foundri-mvp-content/mu-plugins/lib/views/view_loader.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Load Views
+ *
+ * @package   foundri
+ * @author    Josh Pollock <Josh@JoshPress.net>
+ * @license   GPL-2.0+
+ * @link
+ * @copyright 2015 Foundri
+ */
+namespace foundri_mvp_lib\views;
+
+
+class view_loader {
+
+}
