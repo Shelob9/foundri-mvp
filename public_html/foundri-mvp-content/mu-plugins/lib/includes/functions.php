3fca0b30398b340ef75103a9254a308a564dce35
Josh Pollock <jpollock412@gmail.com>
2015-05-12 23:06:15 -0400

    start community single view

--- public_html/foundri-mvp-content/mu-plugins/lib/includes/functions.php
+++ public_html/foundri-mvp-content/mu-plugins/lib/includes/functions.php
@@ -8,3 +8,18 @@
  * @link
  * @copyright 2015 Foundri
  */
+
+function foundri_view() {
+	$id = get_queried_object_id();
+	$view = false;
+	if ( FOUNDRI_COMMUNITY == get_post_type( $id ) ) {
+		$view = 'community-single.html';
+	}
+
+	if ( $view ) {
+		$view = FOUNDDRI_VIEW_DIR . $view;
+
+		return caldera_metaplate_from_file( $view, null, pods( FOUNDRI_COMMUNITY, $id )->export() );
+	}
+
+}
