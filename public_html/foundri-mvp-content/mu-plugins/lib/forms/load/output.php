b270d4a347d1884729311982ce4ecdf2cb7be6a8
Josh Pollock <jpollock412@gmail.com>
2015-05-19 23:03:15 -0400

    finish making user profile save etc.

--- public_html/foundri-mvp-content/mu-plugins/lib/forms/load/output.php
+++ public_html/foundri-mvp-content/mu-plugins/lib/forms/load/output.php
@@ -31,9 +31,36 @@ class output extends form{
 	 */
 	public static function __callStatic( $name, $args ) {
 		if ( self::form_allowed( $name ) ) {
-			return \Caldera_Forms::render_form( $name );
+			$entry_id = self::maybe_entry_id( $name );
+			return \Caldera_Forms::render_form( $name, $entry_id );
 		}
 
+	}
+
+	/**
+	 * Find entry ID if possible and relevant
+	 *
+	 * @since 0.0.1
+	 *
+	 * @access protected
+	 *
+	 * @param string $name Form name
+	 *
+	 * @return int|null Entry ID or null.
+	 */
+	protected static function maybe_entry_id( $name ) {
+		$entry_id = null;
+		if ( 'bio_profile' == $name ) {
+			$_entry_id = get_user_meta( get_current_user_id(), 'profile_cf_entry_id', true );
+			if ( 0 < absint( $_entry_id ) ) {
+				$entry_id = (int) $_entry_id;
+
+			}
+
+
+		}
+
+		return $entry_id;
 	}
 
 
